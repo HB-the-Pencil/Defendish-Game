06c6dbe2eec742b10d6676f5a549dc6afa7cb229
Henry Barcalow <118207101+HB-the-Pencil@users.noreply.github.com>
2024-12-18 15:08:36 -0500

    Install dependencies

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import subprocess
+
+try:
+    subprocess.check_call([sys.executable,
+        "-m", "pip", "install", "-r", "requirements.txt"])
+except subprocess.CalledProcessError:
+    print("Error importing modules")
+
 from defendish import Defendish
 
-d_game = Defendish()
-d_game.run_game()
+if __name__ == "__main__":
+    d_game = Defendish()
+    d_game.run_game()
